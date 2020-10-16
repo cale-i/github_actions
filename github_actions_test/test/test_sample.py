040f093941af3d1ebc9e9b22026dc6504ae60125
cale-i <calei078029@gmail.com>
2020-10-16 18:16:59 +0900

    first commit

---
+++ github_actions_test/test/test_sample.py
@@ -0,0 +1,37 @@
+import unittest
+# from sample import crawl, scrape, print_items
+import sample
+
+
+class TestSample(unittest.TestCase):
+    """
+    test class of sample.py
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        print('test start')
+        cls.html = ""
+
+    @classmethod
+    def tearDownClass(cls):
+        print('test over')
+
+    def test_crawl(self):
+        """
+        test case for crawl
+        """
+        resp = sample.crawl()
+        self.assertEqual(200, resp.status_code)
+        self.html = resp.text
+
+    def test_scrape(self):
+        """
+        test case for crawl
+        """
+        items = sample.scrape(self.html)
+        self.assertIsNotNone(items)
+
+
+if __name__ == "__main__":
+    unittest.main()
